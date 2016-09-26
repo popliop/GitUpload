35186ffb5a3972199b4d92818da1a60f61c5ba9e
Marcus <Marcus@46.236.118.103>
2016-09-26 23:03:47 +0200

    first

---
+++ GitTest/src/main/Main.java
@@ -0,0 +1,16 @@
+package main;
+
+public class Main {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		
+		System.out.println("hej ");
+		System.out.println();
+		//ss
+		System.out.println("hej");
+
+	}
+
+}
